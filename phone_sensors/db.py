f807ef0eed8beb400403a966b05a68bbc608070a
Yibo Wei <david_wyb2001@outlook.com>
2024-04-30 13:27:52 -0700

    Remove unused code

--- phone_sensors/db.py
+++ phone_sensors/db.py
@@ -1,19 +1,9 @@
 """Database initialization"""
 
 from sqlalchemy import Engine
-from sqlmodel import Session, SQLModel
-
-from phone_sensors.schemas import Detection
+from sqlmodel import SQLModel
 
 
 def init_db(engine: Engine) -> None:
     """Initialize the database"""
     SQLModel.metadata.create_all(engine)
-
-
-def insert_detections(engine: Engine, detections: list[Detection]) -> None:
-    """Insert a prediction into the database"""
-    with Session(engine) as session:
-        for detection in detections:
-            session.add(detection)
-        session.commit()
