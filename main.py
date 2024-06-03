e280fc69f7a248a56c7d3ef28df1826712655439
Yibo Wei <david_wyb2001@outlook.com>
2024-06-03 16:19:03 -0700

    Refactor main entry point to use phone_sensors.cli.main()

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 """Main entry point for the application."""
 
-from multiprocessing import Process
-
-import rich
-import uvicorn
-from rq.worker_pool import WorkerPool
-
-from phone_sensors.api import app
-from phone_sensors.settings import get_redis_connection, get_settings
+from phone_sensors.cli import main
 
 if __name__ == "__main__":
-    rich.print(get_settings())
-    api_process = Process(target=uvicorn.run, args=(app,), kwargs={"host": "0.0.0.0", "port": 8000})
-    worker_pool_process = Process(
-        target=WorkerPool(
-            ["default", "high", "low"], num_workers=3, connection=next(get_redis_connection())
-        ).start
-    )
-    api_process.start()
-    worker_pool_process.start()
+    main()
